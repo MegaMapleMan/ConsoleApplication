fbfefced4bead88b3646896ac67cfe5b9c7f858c
Boris Klyon <klen.boris.alekseevich@gmail.com>
2022-02-10 20:39:21 +0400

    Add project files.

---
+++ ConsoleApplication.cpp
@@ -0,0 +1,11 @@
+// ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+    std::cout << DoubleSum(6,4);
+}
+
---
+++ Helpers.h
@@ -0,0 +1,5 @@
+#pragma once
+int DoubleSum(int a, int b) 
+{
+	return a * a + 2 * a * b + b * b;
+}
